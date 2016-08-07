680ed35e39b3e1ee78ca733e92cb0dfa7e70e32a
Hyeonseok Oh <hyeonso5@gmail.com>
2016-08-07 19:39:49 +0900

    Add expire date

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge :  24 * 60 * 60 * 1000}));
 
 app.use('/', routes);
 app.use('/users', users);
